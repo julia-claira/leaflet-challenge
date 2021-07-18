d3f3bb5b520eeab127056167b46c1db0e38164c9
Claira J <julia.claira@gmail.com>
2021-07-18 15:06:58 -0700

    started work on legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,18 +1,20 @@
-// Creating map object
 
-//functions
+
+//Functions
 function depthColor(value){
+    var myColor;
+    if (value<10) myColor="#c3fe0f";
+    else if (value>=10 && value <30)myColor="#fefe0f";
+    else if (value>=30 && value <50)myColor="#fec30f";
+    else if (value>=50 && value <70)myColor="#fe870f";
+    else if (value>=70 && value <90)myColor="#fe4b0f";
+    else {myColor="#ff0f0f"};
 
-    if (value<10)
-    else if (value>=10 && value <30)
-    else if (value>=30 && value <50)
-    else if (value>=50 && value <70)
-    else if (value>=70 && value <90)
-    else 
-
-
-
+    return myColor;
 }
+
+
+// Creating map object
 var myMap = L.map("map", {
     center: [40.7, -95.95],
     zoom: 4
@@ -68,28 +70,36 @@ var myMap = L.map("map", {
     L.circleMarker(earthQuakeInfo[i].location, {
       fillOpacity: 0.75,
       color: "black",
-      fillColor: "purple",
+      fillColor: depthColor(earthQuakeInfo[i].depth),
       weight: 1,
       // Setting our circle's radius equal to the output of our markerSize function
       // This will make our marker's size proportionate to its population
       radius: 2*(1.4*earthQuakeInfo[i].size),
     }).addTo(myMap);//.bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
 }
+
+/*Setup*/
+
+
+/*Legend specific*/
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += "<h4>Depth</h4>";
+  div.innerHTML += '<i style="background: #c3fe0f"></i><span>-10 to 10</span><br>';
+  div.innerHTML += '<i style="background: #fefe0f"></i><span>10 to 30</span><br>';
+  div.innerHTML += '<i style="background: #fec30f"></i><span>30 to 50</span><br>';
+  div.innerHTML += '<i style="background: #fe870f"></i><span>50 to 70l</span><br>';
+  div.innerHTML += '<i style="background: #fe4b0f"></i><span>70 to 90</span><br>';
+  div.innerHTML += '<i style="background: #ff0f0f"></i><span>90+</span><br>';
+  div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span></span><br>';
+  
+  
+
+  return div;
+};
+
+legend.addTo(myMap);
+
 })
-/*
-    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
-  
-      // Add min & max
-      var legendInfo = "<h1>Median Income</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
-  
-      div.innerHTML = legendInfo;*/
