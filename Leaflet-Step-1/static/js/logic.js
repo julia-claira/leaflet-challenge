1baacd6c4fb859d200c9d20af06302c03bb1f702
Claira J <julia.claira@gmail.com>
2021-07-18 21:02:51 -0700

    cleaned up js for readability

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,4 @@
-
-
-//Functions
+//Function for depth color
 function depthColor(value){
     var myColor;
     if (value<10) myColor="#c3fe0f";
@@ -13,111 +11,83 @@ function depthColor(value){
     return myColor;
 }
 
-
-
 // Creating map object
 var myMap = L.map("map", {
     center: [35.7, -95.95],
     zoom: 4,
-  });
+    });
   
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/light-v10",
     accessToken: API_KEY
-  }).addTo(myMap);
+    }).addTo(myMap);
   
-  // Store API query variables
-  var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  var plates="PB2002_plates.json";
-  //var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-  //var complaint = "&complaint_type=Rodent";
-  //var limit = "&$limit=10000";
-  d3.json(plates).then(function(response) {
-        console.log(response);
-  });
-  // Assemble API query URL
-  //var url = baseURL + date + complaint + limit;
+// Store API query variables
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+var plates="PB2002_plates.json";
+//call for tectonic plates if I do the optional part
+/*d3.json(plates).then(function(response) {
+    console.log(response);
+});*/
   
-  // Grab the data with d3
-  d3.json(baseURL).then(function(response) {
+// Grab the data with d3
+d3.json(baseURL).then(function(response) {
   
     // Create a new marker cluster group
-
     var earthQuakeInfo=[];
-    // Loop through data
+    
     for (var i = 0; i < response.features.length; i++) {
-  
-      // Set the data location property to a variable
-      var location = response.features[i].geometry;
-      var size = response.features[i].properties.mag;
-  
-      // Check for location property
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
+        var location = response.features[i].geometry;
+        var size = response.features[i].properties.mag;
+        // Check for location property
+        if (location) {
+        // Add a new marker to the cluster group
         earthQuakeInfo.push({'location':[location.coordinates[1], location.coordinates[0]],
             'depth':location.coordinates[2],'size':size});
-      };
-
-    
-
+        };
     }
 
-    // Loop through the cities array and create one marker for each city object
+    // Loop through the earthquakes array and create one marker for each city object
     for (var i = 0; i < earthQuakeInfo.length; i++) {
     
-    L.circleMarker(earthQuakeInfo[i].location, {
-      fillOpacity: 0.75,
-      color: "black",
-      fillColor: depthColor(earthQuakeInfo[i].depth),
-      weight: 1,
-      // Setting our circle's radius equal to the output of our markerSize function
-      // This will make our marker's size proportionate to its population
-      radius: 2*(1.7*earthQuakeInfo[i].size),
-    }).bindPopup("<h2>" + earthQuakeInfo[i].location + "</h2> <hr>" +"<h3>Magnitude: " + 
-        earthQuakeInfo[i].size + "</h3>" +"<h3>Depth: " + 
-        earthQuakeInfo[i].depth + "</h3>").addTo(myMap);
-}
+        L.circleMarker(earthQuakeInfo[i].location, {
+            fillOpacity: 0.75,
+            color: "black",
+            fillColor: depthColor(earthQuakeInfo[i].depth),//calls the color function for depth
+            weight: 1,
+            // This will make our marker's size proportionate to magnitude
+            radius: 2*(1.7*earthQuakeInfo[i].size),
+         }).bindPopup("<h2>" + earthQuakeInfo[i].location + "</h2> <hr>" +"<h3>Magnitude: " + 
+            earthQuakeInfo[i].size + "</h3>" +"<h3>Depth: " + 
+            earthQuakeInfo[i].depth + "</h3>").addTo(myMap);
+    };
 
-/*Setup*/
+    //Legend
+    var legend = L.control({position: 'bottomright'});
 
-  
-  // Create a control for our layers, add our overlay layers to it
-  L.control.layers(null, overlays).addTo(myMap);
-  
-  // Create a legend to display information about our map
-  var info = L.control({
-    position: "bottomleft"
-  });
+    legend.onAdd = function (map) {
 
-  
+        var div = L.DomUtil.create('div', 'info legend'),
+            depths = [-10, 10, 30, 50, 70, 90],
+            labels = [];
 
+        // loop through our density intervals and generate a label with a colored square for each interval
+        div.innerHTML="<h4>Depth</h4>"
+        for (var i = 0; i < depths.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + depthColor(depths[i] + 1) + '"></i> ' +
+                depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+        }
 
-/*Legend specific*/
-var legend = L.control({position: 'bottomright'});
+        return div;
+    };
 
-legend.onAdd = function (map) {
+    legend.addTo(myMap);
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [-10, 10, 30, 50, 70, 90],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    div.innerHTML="<h4>Depth</h4>"
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + depthColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(myMap);
-
-})
+}); 
